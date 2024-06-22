53170a83dc257164c6ad853241bc188652d68b76
unknownwyj <54095369+unknownwyj@users.noreply.github.com>
2024-06-22 11:27:52 +0800

    Update index.php

--- src/index.php
+++ src/index.php
@@ -1,7 +1,28 @@
 <?php
-
-echo "Hello, World from Docker! <br>";
-echo "Hello ECS! <br>";
-echo '<img src="https://www.docker.com/sites/default/files/horizontal.png">';
-
+Try {
+        $url = 'https://dynamicdns.park-your-domain.com/update?host='.$_GET['host'].'&domain='.$_GET['domain'].'&password='.$_GET['password'].'&ip='.$_GET['ip'];
+        $req = curl_init();
+        curl_setopt($req, CURLOPT_URL, $url);
+        curl_setopt($req, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-type: text/xml'));
+        $res = curl_exec($req);
+        curl_close($req);
+        $xml = new SimpleXMLElement(trim(str_replace("encoding=\"utf-16\"","",$res)));
+        if ($xml->ErrCount > 0) {
+                $error = $xml->errors[0]->Err1;
+                if (strcmp($error, "Domain name not found") === 0) {
+                          echo "nohost";
+                } elseif (strcmp($error, "Passwords do not match") === 0) {
+                          Echo "Badauth";
+                } elseif (strcmp($error, "No Records updated. A record not Found;") === 0) {
+                          echo "nohost";
+                } else {
+                     echo "911 [".$error."]";
+                }
+        } else {
+                  Echo "Good";
+        }
+} catch (Exception $e) {
+      echo "911 [".$e->getMessage()."]";
+}
 ?>
